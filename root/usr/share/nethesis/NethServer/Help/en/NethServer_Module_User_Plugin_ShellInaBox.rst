4a8966afa1e133ee99a069d9a0f302a208e2d31e
Stephane de Labrusse <stephdl@de-labrusse.fr>
2016-11-08 01:58:24 +0100

    ns7 work

--- root/usr/share/nethesis/NethServer/Help/en/NethServer_Module_User_Plugin_ShellInaBox.rst
+++
@@ -1,6 +0,0 @@
-.. --initial-header-level=3 
-
-Shellinabox Ajax Terminal
-^^^^^^^^^^^^^^^^^^^^^^^^^
-
-This user is allowed to use shellinabox through the Apache authentication.
--- root/usr/share/nethesis/NethServer/Template/User/Plugin/ShellInaBox.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo $view->fieldset()->setAttribute('template', $T('ShellinaboxPermissionAccess_label'))
-->insert($view->checkBox('ShellinaboxUser','enabled')->setAttribute('uncheckedValue', 'disabled'));
